fd466e46143e949ef43f4a2fbeaaad9d50e3a19d
isaaq <prec@163.com>
2024-12-21 00:40:13 +0800

    fix: clz not found error

--- lib/ui/ui_impl/layui/layui_transformer.rb
+++ lib/ui/ui_impl/layui/layui_transformer.rb
@@ -4,12 +4,15 @@ class LayuiTransformer
   DICT = { :'t-col' => :TableColTarget, :'t-table' => :TableTarget, i: :InputTarget, f: :FormTarget }
   def self.trans(tag, ctx, children)
     clzname = DICT[tag.name.to_sym]
-    if Object.const_defined?(clzname)
+    if !clzname.nil? && Object.const_defined?(clzname)
       ele = Object.const_get(clzname).new
       ele.tag = tag
       ele.context = ctx
       # ele.object_tree = fnd
       ele.children = children
+    else
+      __p tag.name
+      throw
     end
     ele.output
   end
--- test/test_ui.rb
+++ test/test_ui.rb
@@ -12,12 +12,9 @@ class TestModel < Test::Unit::TestCase
       <kr:form>
         <kr:input lbl="账号" name="username" is_required="true"/>
       </kr:form>
-
-      <l:t>
-      </l:t>
     CODE
     # code = UIPage.new(:kr).parse(code)
-    code = "//[{\"type\":\"form\",\"id\":\"form_8fe2f66a\",\"input\":[{\"type\":\"input\",\"lbl\":\"账号\",\"name\":\"username\",\"is_required\":\"true\",\"id\":\"input_017e9583\"}]}]//\n<l:f>\n  //[null]//\n<l:i name=\"username\" lbl=\"账号\" is_required=\"true\" />\n</l:f>\n\n<l:t>\n</l:t>\n"
+    # code = "//[{\"type\":\"form\",\"id\":\"form_8fe2f66a\",\"input\":[{\"type\":\"input\",\"lbl\":\"账号\",\"name\":\"username\",\"is_required\":\"true\",\"id\":\"input_017e9583\"}]}]//\n<l:f>\n  //[null]//\n<l:i name=\"username\" lbl=\"账号\" is_required=\"true\" />\n</l:f>\n\n<l:t>\n</l:t>\n"
     code = "<l:f></l:f>"
     code2 = UIPage.new(:layui).parse(code)
     p code2
