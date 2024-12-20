d2011a3b625fe76f5922c1c5f8d1c8153b695187
isaaq <prec@163.com>
2024-12-20 17:36:31 +0800

    feat: 基础标签处理

---
+++ lib/ui/ui_impl/layui/target/form/form_target.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+##
+# FormTarget
+# Form的Layui目标类
+#
+
+class FormTarget < LayuiElementTarget
+
+    def output_target
+
+    end
+
+end
---
+++ lib/ui/ui_impl/layui/target/form/input_target.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class InputTarget < LayuiElementTarget
+    def elename
+        'form'
+    end
+
+    def output
+        
+    end
+end 
